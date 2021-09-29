ca51c6211eb63ac46d9721768e964a5751a8fe72
Verisana <account@0112.pro>
2021-09-29 18:10:27 +0500

    refactor: remove all console.log() from deploy util

--- src/utils/deploy.ts
+++ src/utils/deploy.ts
@@ -1,12 +1,9 @@
 import hre from "hardhat";
+import { Contract } from "ethers";
 
-export async function deploy(contractName: string) {
+export async function deploy(contractName: string): Promise<Contract> {
     const ContractFactory = await hre.ethers.getContractFactory(contractName);
-    const Contract = await ContractFactory.deploy();
-    console.log("Deploy transaction sent");
-    await Contract.deployed();
-
-    console.log(
-        `Contract deployed on ${hre.network.name} to: ${Contract.address}`
-    );
+    const contract = await ContractFactory.deploy();
+    await contract.deployed();
+    return contract;
 }
