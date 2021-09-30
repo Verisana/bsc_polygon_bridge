03b79c237866a8040c87a40d30f2e9bebf2718f0
Verisana <account@0112.pro>
2021-09-30 15:18:26 +0500

    feat: write main Bridge functionality

--- src/contracts/Bridge.sol
+++ src/contracts/Bridge.sol
@@ -1,22 +1,162 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity >=0.8.4;
 
+import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
 import {IBridge} from "./interfaces/IBridge.sol";
+import {NFT} from "./NFT.sol";
+import "./libraries/SignatureChecker.sol";
 
-contract Bridge is IBridge {
-    constructor() {}
+contract Bridge is
+    IBridge,
+    IERC721Receiver,
+    Ownable,
+    ReentrancyGuard,
+    Pausable
+{
+    NFT private _NFTContract;
+    ChainName internal _chainFrom;
+    ChainName internal _chainTo;
 
-    function initSwap(uint256 tokenId) external view override {}
+    mapping(bytes32 => SwapDetail) public eventStore;
+
+    mapping(address => uint256) public nonceStore;
+
+    constructor(address addressNFT, ChainName chainFrom) {
+        _NFTContract = NFT(addressNFT);
+        _chainFrom = chainFrom;
+        _chainTo = _chainFrom == ChainName.POLYGON
+            ? ChainName.BSC
+            : ChainName.POLYGON;
+    }
+
+    function onERC721Received(
+        address,
+        address,
+        uint256,
+        bytes memory
+    ) public virtual override returns (bytes4) {
+        // Here we can create some bumping mechanism to prevent stuck tokens.
+        // But I'm not sure when this function is called. It's delayed till better times
+
+        return this.onERC721Received.selector;
+    }
+
+    function calculateHash(SwapDetail memory swapDetail)
+        public
+        pure
+        returns (bytes32)
+    {
+        return
+            keccak256(
+                abi.encode(
+                    swapDetail.sender,
+                    swapDetail.tokenId,
+                    swapDetail.chainFrom,
+                    swapDetail.chainTo,
+                    swapDetail.nonce
+                )
+            );
+    }
+
+    function isInEventStore(bytes32 eventHash) public view returns (bool) {
+        if (eventStore[eventHash].isValue) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    function initSwap(uint256 tokenId)
+        external
+        payable
+        override
+        nonReentrant
+        whenNotPaused
+    {
+        _NFTContract.safeTransferFrom(msg.sender, address(this), tokenId);
+        require(_NFTContract.ownerOf(tokenId) == address(this));
+
+        SwapDetail memory swapDetail = SwapDetail({
+            sender: msg.sender,
+            tokenId: tokenId,
+            chainFrom: _chainFrom,
+            chainTo: _chainTo,
+            nonce: nonceStore[msg.sender],
+            isValue: true
+        });
+
+        bytes32 eventHash = calculateHash(swapDetail);
+
+        require(
+            isInEventStore(eventHash) == false,
+            "eventHash must not be initialized"
+        );
+
+        eventStore[eventHash] = swapDetail;
+
+        emit InitSwap(
+            swapDetail.sender,
+            swapDetail.tokenId,
+            swapDetail.chainFrom,
+            swapDetail.chainTo,
+            swapDetail.nonce
+        );
+        nonceStore[msg.sender]++;
+    }
 
     function redeemSwap(
         address sender,
         uint256 tokenId,
-        string memory chainFrom,
-        string memory chainTo,
+        ChainName chainFrom,
+        ChainName chainTo,
         uint256 nonce,
         uint8 v,
         bytes32 r,
-        bytes32 s,
-        bytes32 hash
-    ) external view override {}
+        bytes32 s
+    ) external payable override nonReentrant whenNotPaused {
+        SwapDetail memory swapDetail = SwapDetail({
+            sender: sender,
+            tokenId: tokenId,
+            chainFrom: chainFrom,
+            chainTo: chainTo,
+            nonce: nonce,
+            isValue: true
+        });
+        bytes32 eventHash = calculateHash(swapDetail);
+
+        require(
+            isInEventStore(eventHash) == false,
+            "eventHash must not be initialized"
+        );
+
+        eventStore[eventHash] = swapDetail;
+
+        require(
+            SignatureChecker.isValidSignatureNow(
+                this.owner(),
+                eventHash,
+                v,
+                r,
+                s
+            ),
+            "redeemSwap: invalid signature"
+        );
+
+        _NFTContract.safeTransferFrom(
+            address(this),
+            swapDetail.sender,
+            swapDetail.tokenId
+        );
+
+        emit RedeemSwap(
+            swapDetail.sender,
+            swapDetail.tokenId,
+            swapDetail.chainFrom,
+            swapDetail.chainTo,
+            swapDetail.nonce
+        );
+    }
 }
--- src/contracts/interfaces/IBridge.sol
+++ src/contracts/interfaces/IBridge.sol
@@ -2,33 +2,48 @@
 pragma solidity >=0.8.4;
 
 interface IBridge {
+    struct SwapDetail {
+        address sender;
+        uint256 tokenId;
+        ChainName chainFrom;
+        ChainName chainTo;
+        uint256 nonce;
+        bool isValue;
+    }
+
+    enum ChainName {
+        BSC,
+        POLYGON
+    }
+
     event InitSwap(
         address sender,
         uint256 tokenId,
-        string chainFrom,
-        string chainTo,
+        ChainName chainFrom,
+        ChainName chainTo,
         uint256 nonce
     );
 
     event RedeemSwap(
         address sender,
         uint256 tokenId,
-        string chainFrom,
-        string chainTo,
+        ChainName chainFrom,
+        ChainName chainTo,
         uint256 nonce
     );
 
-    function initSwap(uint256 tokenId) external view;
+    event FinalizeSwap(bytes32 hashEvent);
+
+    function initSwap(uint256 tokenId) external payable;
 
     function redeemSwap(
         address sender,
         uint256 tokenId,
-        string memory chainFrom,
-        string memory chainTo,
+        ChainName chainFrom,
+        ChainName chainTo,
         uint256 nonce,
         uint8 v,
         bytes32 r,
-        bytes32 s,
-        bytes32 hash
-    ) external view;
+        bytes32 s
+    ) external payable;
 }
