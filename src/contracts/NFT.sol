169c303476c48435a11744f6435dbaa5b4e69a69
Verisana <account@0112.pro>
2021-09-30 15:17:15 +0500

    feat: make burn method available only to Minter

--- src/contracts/NFT.sol
+++ src/contracts/NFT.sol
@@ -7,4 +7,21 @@ import "@openzeppelin/contracts/token/ERC721/presets/ERC721PresetMinterPauserAut
 // use abstract name
 contract NFT is ERC721PresetMinterPauserAutoId {
     constructor() ERC721PresetMinterPauserAutoId("NoPurposeNFT", "NPNFT", "") {}
+
+    /**
+     * @dev Burns `tokenId`. See {ERC721-_burn}.
+     *
+     * Restrict token burning by user. We should enforce tokenId 1 to 1 match between
+     * two blockchains
+     *
+     * Requirements:
+     *
+     * - The caller must own `tokenId` or be an approved operator.
+     * - And have MINTER_ROLE access
+     */
+    function burn(uint256 tokenId) public override virtual onlyRole(MINTER_ROLE) {
+        //solhint-disable-next-line max-line-length
+        require(_isApprovedOrOwner(_msgSender(), tokenId), "ERC721Burnable: caller is not owner nor approved");
+        _burn(tokenId);
+    }
 }
