1f9bf7e8ef468794b3d1e6d93707b9c128ff05d3
Verisana <account@0112.pro>
2021-09-30 15:15:39 +0500

    feat: make deploy_bridge with appropriate params

--- src/scripts/deploy_bridge.ts
+++ src/scripts/deploy_bridge.ts
@@ -1,10 +1,28 @@
+import * as dotenv from "dotenv";
+
 import hre from "hardhat";
 
 import { deploy } from "../utils/deploy";
+import { getEnvVariable } from "../utils/utils";
+
+dotenv.config();
+
+const binanceNFTAddress = getEnvVariable("BSC_NFT_ADDRESS");
+const polygonNFTAddress = getEnvVariable("POLYGON_NFT_ADDRESS");
 
 async function main() {
     const contractName = "Bridge";
-    const contract = await deploy(contractName);
+
+    let contract;
+    if (hre.network.name === "testnet") {
+        contract = await deploy(contractName, binanceNFTAddress, "BSC");
+    } else if (hre.network.name === "mumbai") {
+        contract = await deploy(contractName, polygonNFTAddress, "POLYGON");
+    } else {
+        throw new Error(
+            `Unknown network "${hre.network.name}" to deploy ${contractName}`
+        );
+    }
     console.log(
         `${contractName} deployed on ${hre.network.name} to: ${contract.address}`
     );
