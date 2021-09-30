6403691d9c11ac570b3866993f54651b3a5692f3
Verisana <account@0112.pro>
2021-09-30 15:14:50 +0500

    feat: update deploy util to receive args

--- src/utils/deploy.ts
+++ src/utils/deploy.ts
@@ -1,9 +1,12 @@
 import hre from "hardhat";
 import { Contract } from "ethers";
 
-export async function deploy(contractName: string): Promise<Contract> {
+export async function deploy(
+    contractName: string,
+    ...args: unknown[]
+): Promise<Contract> {
     const ContractFactory = await hre.ethers.getContractFactory(contractName);
-    const contract = await ContractFactory.deploy();
+    const contract = await ContractFactory.deploy(args);
     await contract.deployed();
     return contract;
 }
